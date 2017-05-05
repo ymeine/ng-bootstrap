3d352443a0976850fdbf4cc78e6ed01720accb93
ymeine <yannick.meine@gmail.com>
2017-05-05 09:50:07 +0200

    WIP

--- src/alert/alert.ts
+++ src/alert/alert.ts
@@ -4,6 +4,12 @@ import {
   Output,
   EventEmitter,
   ChangeDetectionStrategy,
+
+  trigger,
+  state,
+  style,
+  animate,
+  transition
 } from '@angular/core';
 
 import {NgbAlertConfig} from './alert-config';
@@ -15,13 +21,38 @@ import {NgbAlertConfig} from './alert-config';
   selector: 'ngb-alert',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div [class]="'alert alert-' + type + (dismissible ? ' alert-dismissible' : '')" role="alert">
+    <div
+      [class]="'alert alert-' + type + (dismissible ? ' alert-dismissible' : '') + (false && visibilityState === 'shown' ? ' show' : '')"
+      [@visibility]="visibilityState"
+      role="alert"
+    >
       <button *ngIf="dismissible" type="button" class="close" aria-label="Close" (click)="closeHandler()">
             <span aria-hidden="true">&times;</span>
       </button>
       <ng-content></ng-content>
     </div>
-    `
+    `,
+    animations: [
+      trigger('visibility', [
+        state('shown', style({
+          opacity: '*'
+          //height: 100
+        })),
+        /*state('hidden', style({
+          opacity: '0'
+        })),
+        transition('shown => hidden', animate('0.15s linear')),*/
+        transition(':leave', animate('2s linear', style({
+            //height: 0
+            opacity: 0
+        })))
+      ])
+    ],
+    host: {
+      //'[@visibility]': 'visibilityState'
+      /*'(@visibility.done)': 'animation_callback("done", $event)',
+      '(@visibility.start)': 'animation_callback("start", $event)'*/
+    }
 })
 export class NgbAlert {
   /**
@@ -38,10 +69,22 @@ export class NgbAlert {
    */
   @Output() close = new EventEmitter();
 
+  private visibilityState: string = 'shown';
+
   constructor(config: NgbAlertConfig) {
     this.dismissible = config.dismissible;
     this.type = config.type;
   }
 
-  closeHandler() { this.close.emit(null); }
+  closeHandler() {
+    this.visibilityState = 'hidden';
+    this.close.emit(null);
+  }
+
+  animation_callback(step, event) {
+    console.log(step, event, this);
+    /*if (step === 'start') {
+      this.
+    }*/
+  }
 }
