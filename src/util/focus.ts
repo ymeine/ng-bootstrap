00b44172e547a76496cf3e7ad86629bfe3f5a9c2
ymeine <yannick.meine@gmail.com>
2017-11-24 15:07:01 +0100

    feat(modal): navigation trap
    Added support for aria-hidden

--- src/util/focus.ts
+++ src/util/focus.ts
@@ -89,7 +89,34 @@ export function createFocusInterceptor({onIntercept}) {
   return element;
 }
 
-export function trapFocusInside(element: HTMLElement) {
+export function hideOtherElements(element: HTMLElement) {
+  const attribute = 'aria-hidden';
+  const attributeValue = 'true';
+
+  const {body} = document;
+  const parent = element.parentElement;
+
+  const hiddenElements =
+      Array.from(parent.children).filter(child => child !== element && !isDefined(child.getAttribute(attribute)));
+  hiddenElements.forEach(hiddenElement => hiddenElement.setAttribute(attribute, attributeValue));
+
+  const revertParentSiblings = parent === body ? () => {} : hideOtherElements(parent);
+
+  const revert = () => {
+    hiddenElements.forEach(hiddenElement => hiddenElement.removeAttribute(attribute));
+    revertParentSiblings();
+  };
+
+  return revert;
+}
+
+export interface InterceptorDescription {
+  anchor: HTMLElement;
+  position: InsertPosition;
+  setFocus: (element: HTMLElement) => any;
+}
+
+export function trapFocusInside(element: HTMLElement): () => any {
   const {body} = document;
   const interceptors = [
     {anchor: element, position: 'beforebegin', setFocus: focusLast},
@@ -102,8 +129,11 @@ export function trapFocusInside(element: HTMLElement) {
     return interceptor;
   });
 
+  const revertHiddenElements = hideOtherElements(element);
+
   const revert = () => {
     interceptors.forEach(interceptor => interceptor.remove());
+    revertHiddenElements();
   };
 
   return revert;
