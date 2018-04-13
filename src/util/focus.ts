72ca7ab2d635a4670f20b613cad414fe39eb5d35
ymeine <yannick.meine@gmail.com>
2018-04-13 19:19:20 +0200

    feat(modal): navigation trap
    optimized filtering by using lazy generators

--- src/util/focus.ts
+++ src/util/focus.ts
@@ -3,12 +3,60 @@ import {isDefined} from './util';
 
 
 ////////////////////////////////////////////////////////////////////////////////
+// Iterating
+////////////////////////////////////////////////////////////////////////////////
+
+export function* forwardIndex(size: number): IterableIterator<number> {
+  for (let index = 0; index < size; index++) { yield index; }
+}
+
+export function* backwardIndex(size: number): IterableIterator<number> {
+  for (let index = size - 1; index >= 0; index--) { yield index; }
+}
+
+export function indexGenerator(size: number, reverse?: boolean): IterableIterator<number> {
+  return (!reverse ? forwardIndex : backwardIndex)(size);
+}
+
+export interface SpecFilteredCollectionIterator<T> {
+  collection: T[];
+  reverse: boolean;
+}
+
+export type CollectionIteratorFilter<T, M = T> = (item: T, index: number, collection: T[]) => {exclude: boolean, value: M};
+
+export function* filteredCollectionIterator<T, M = T>(
+  {collection, reverse}: SpecFilteredCollectionIterator<T>,
+  filter: CollectionIteratorFilter<T, M>
+) {
+  for (let index of indexGenerator(collection.length, reverse)) {
+    const {exclude, value} = filter(collection[index], index, collection);
+    if (!exclude) { yield value; }
+  }
+}
+
+export function getFirst<T, M = T>(collection: T[], filter: CollectionIteratorFilter<T, M>): M {
+  return filteredCollectionIterator<T, M>({collection, reverse: false}, filter).next().value;
+}
+
+export function getLast<T, M = T>(collection: T[], filter: CollectionIteratorFilter<T, M>): M {
+  return filteredCollectionIterator<T, M>({collection, reverse: true}, filter).next().value;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Focusing
 ////////////////////////////////////////////////////////////////////////////////
 
 export function getTabIndex(element: HTMLElement): number {
   const value = element.getAttribute('tabindex');
   return isDefined(value) ? parseInt(value, 10) : element.tabIndex;
+}
+
+export function getTabIndexForSorting(element: HTMLElement): number {
+  const value = getTabIndex(element);
+  return value === 0 ? Infinity : value;
 }
 
 export function isElementNotDisplayed(element: any, notDisplayedCache: Map<HTMLElement, boolean>): boolean {
@@ -41,11 +89,6 @@ export function isElementHiddenOrDisabled(element: any, notDisplayedCache: Map<H
   return false;
 }
 
-export function getTabIndexForSorting(element: HTMLElement): number {
-  const value = getTabIndex(element);
-  return value === 0 ? Infinity : value;
-}
-
 export interface TabbableWrapper {
   element: HTMLElement;
   index: number;
@@ -54,37 +97,36 @@ export interface TabbableWrapper {
 
 export interface SpecFilterTabbable {
   wrapper: TabbableWrapper;
-  excludeHighTabIndexes?: boolean;
-  excludeNaturalTabIndexes?: boolean;
   notDisplayedCache: Map<HTMLElement, boolean>;
 };
 
-function filterPotentialTabbable({wrapper, notDisplayedCache}: SpecFilterTabbable) {
+function isActualTabbable({wrapper, notDisplayedCache}: SpecFilterTabbable) {
   const {element, tabIndex} = wrapper;
 
   if (tabIndex < 0) { return false; }
   if (tabIndex !== Infinity) { return false; }
 
-  if (isElementHiddenOrDisabled(wrapper.element, notDisplayedCache)) { return false; }
+  if (isElementHiddenOrDisabled(element, notDisplayedCache)) { return false; }
 
   return true;
 }
 
-export interface SpecGetTabbable {
-  root: HTMLElement;
-};
-
-export function getTabbable({root}: SpecGetTabbable): HTMLElement[] {
+function potentialTabbableFilter() {
   const notDisplayedCache = new Map();
+
+  return (element, index) => ({
+    value: element,
+    exclude: !isActualTabbable({wrapper: {element, index, tabIndex: getTabIndexForSorting(element)}, notDisplayedCache}),
+  });
+}
+
+export function getPotentialTabbable(root: HTMLElement): HTMLElement[] {
   return Array.from(root.querySelectorAll([
     'input', 'select', 'button',
     'a[href]', 'area[href]',
     'textarea',
     '[tabindex]'
-  ].join(', ')))
-  .map((element: HTMLElement, index: number): TabbableWrapper => { return {element, index, tabIndex: getTabIndexForSorting(element) }; })
-  .filter(wrapper => filterPotentialTabbable({wrapper, notDisplayedCache}))
-  .map(wrapper => wrapper.element);
+  ].join(', ')));
 }
 
 export interface SpecFindFocusable {
@@ -96,8 +138,8 @@ export interface SpecFindFirstFocusable extends SpecFindFocusable {
 }
 
 export function findFirstFocusable(spec: SpecFindFirstFocusable): HTMLElement {
-  const children = getTabbable(spec);
-  return children[spec.reverse ? children.length - 1 : 0];
+  const getter = spec.reverse ? getLast : getFirst;
+  return getter<HTMLElement>(getPotentialTabbable(spec.root), potentialTabbableFilter());
 }
 
 export function focusFirst(spec: SpecFindFirstFocusable) {
