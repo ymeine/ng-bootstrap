843ae5b394eeb0a84d10cc1327fe39d652090edb
ymeine <yannick.meine@gmail.com>
2018-07-20 11:28:54 +0200

    fix(modal): adjust modal background to avoid shifting
    changes after review

--- src/util/scrollbar.ts
+++ src/util/scrollbar.ts
@@ -2,20 +2,48 @@ import {Injectable, Inject} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 
 
+const noop = () => {};
 
+
+
+/** Type for the callback used to revert the scrollbar compensation. */
+export type CompensationReverter = () => void;
+
+
+
+/**
+ * Utility to handle the scrollbar.
+ *
+ * It allows to compensate the lack of a vertical scrollbar by adding an
+ * equivalent padding on the right of the body, and to remove this compensation.
+ */
 @Injectable()
 export class ScrollBar {
+  private _width: number = null;
+
   constructor(@Inject(DOCUMENT) private _document) {}
 
-  compensate() {
+  /**
+   * Detects if a scrollbar is present and if yes, already compensates for its
+   * removal by adding an equivalent padding on the right of the body.
+   *
+   * @return a callback used to revert the compensation (noop if there was none,
+   * otherwise a function removing the padding)
+   */
+  compensate(): CompensationReverter {
     if (!this._isPresent()) {
-      return () => {};
+      return noop;
     }
     const width = this._getWidth();
     return this._adjustBody(width);
   }
 
-  private _adjustBody(width: number) {
+  /**
+   * Adds a padding of the size of a scrollbar on the right of the body.
+   *
+   * @return a callback used to remove this padding
+   */
+  private _adjustBody(width: number): CompensationReverter {
     const {body} = this._document;
     const userSetPadding = body.style.paddingRight;
     const paddingAmount = parseFloat(window.getComputedStyle(body)['padding-right']);
@@ -23,12 +51,26 @@ export class ScrollBar {
     return () => body.style['padding-right'] = userSetPadding;
   }
 
+  /**
+   * Tells whether a scrollbar is currently present on the body.
+   *
+   * @return true if scrollbar is present, false otherwise
+   */
   private _isPresent(): boolean {
     const rect = this._document.body.getBoundingClientRect();
     return rect.left + rect.right < window.innerWidth;
   }
 
+  /**
+   * Calculates and returns the width of a scrollbar. Result is cached.
+   *
+   * @return the width of a scrollbar on this page
+   */
   private _getWidth(): number {
+    if (this._width != null) {
+      return this._width;
+    }
+
     const document = this._document;
     const {body} = document;
 
@@ -39,6 +81,7 @@ export class ScrollBar {
     const width = measurer.getBoundingClientRect().width - measurer.clientWidth;
     body.removeChild(measurer);
 
+    this._width = width;
     return width;
   }
 }
