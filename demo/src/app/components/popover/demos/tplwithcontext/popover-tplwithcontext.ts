8260f274b0f87f91617e5720572ca2929add09a7
ymeine <yannick.meine@gmail.com>
2018-07-27 16:37:58 +0200

    fix(demo): fix closing of popover & tooltip using popup context
    Changes after review

--- demo/src/app/components/popover/demos/tplwithcontext/popover-tplwithcontext.ts
+++ demo/src/app/components/popover/demos/tplwithcontext/popover-tplwithcontext.ts
@@ -1,22 +1,17 @@
-import {NgbPopover} from '@ng-bootstrap/ng-bootstrap';
-import {Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-popover-tplwithcontext',
   templateUrl: './popover-tplwithcontext.html'
 })
 export class NgbdPopoverTplwithcontext {
-  greeting: string;
   name = 'World';
 
-  @ViewChild('p') public popover: NgbPopover;
-
-  public changeGreeting(greeting: string): void {
-    const isOpen = this.popover.isOpen();
-    this.popover.close();
-    if (greeting !== this.greeting || !isOpen) {
-      this.greeting = greeting;
-      this.popover.open({greeting});
+  toggleWithGreeting(popover, greeting: string) {
+    if (popover.isOpen()) {
+      popover.close();
+    } else {
+      popover.open({greeting});
     }
   }
 }
