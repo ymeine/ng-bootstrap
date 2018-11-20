f6b2eed77cc698c3bd686e488f84a7501188be6d
ymeine <yannick.meine@gmail.com>
2018-11-20 14:48:10 +0100

    docs(typeahead): overview
    wrote more documentation

--- demo/src/app/components/typeahead/overview/sections/introduction/index.ts
+++ demo/src/app/components/typeahead/overview/sections/introduction/index.ts
@@ -3,6 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngbd-typeahead-overview-section-introduction',
   templateUrl: './template.html',
+  styles: [
+    `
+    .defined {
+      font-weight: bold;
+      font-style: italic;
+      text-decoration: underline;
+    }
+    `
+  ],
 })
 export class NgbdTypeaheadOverviewSectionIntroductionComponent {
 }
